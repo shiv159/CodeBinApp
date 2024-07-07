eddbf80145522f10f9d39f2df875951c598d892e
shiv159 <zaynster007@gmail.com>
2024-07-07 17:42:51 +0530

    code bin component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { CreatebinComponent } from './components/createbin/createbin.component';
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
+    {path:'create',component:CreatebinComponent},
     {path: 'signup',component:SignupComponent},
-    {path: '',redirectTo: '/login',pathMatch: 'full'}
+    {path: '',redirectTo: '/login',pathMatch: 'full'},
+    {path: '**',component:NotfoundComponent}
 ];
